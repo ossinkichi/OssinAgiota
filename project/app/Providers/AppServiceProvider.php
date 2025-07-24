d81790d8c673aec0a8d6158d89414f1cb20cf276
Ossin Kichi <loboresjunior321@gmail.com>
2025-07-24 17:23:55 -0300

    Adicionar diretiva Blade 'viteOrFallback' e atualizar a página de boas-vindas para usar a nova diretiva. Remover arquivo de layout de formulário.

--- project/app/Providers/AppServiceProvider.php
+++ project/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
@@ -23,5 +24,16 @@ class AppServiceProvider extends ServiceProvider
         Route::prefix('api')
             ->middleware('api')
             ->group(base_path('routes/api.php'));
+
+        Blade::directive('viteOrFallback', function ($expression) {
+            return "<?php
+            if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot'))) {
+                echo vite($expression);
+            } else {
+                echo '<link rel=\"stylesheet\" href=\"' . asset('css/tailwind.css') . '\">';
+                echo '<script src=\"' . asset('js/app.js') . '\" defer></script>';
+            }
+            ?>";
+        });
     }
 }
