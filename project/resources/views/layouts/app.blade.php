e6eb2773cc59ce8d9a124cc9f910565a96f7a15b
Ossin Kichi <loboresjunior321@gmail.com>
2025-07-26 01:31:23 -0300

    Ajustar a formatação do layout principal, organizando a indentação e a estrutura do código.

--- project/resources/views/layouts/app.blade.php
+++ project/resources/views/layouts/app.blade.php
@@ -1,26 +1,29 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ @yield('title') }}</title>
+    <title>{{ @(yield 'title') }}</title>
 </head>
+
 <body>
     <header>
-    @include('partials.logo')
+        @include('partials.logo')
 
-    @if (isset($slot) && $slot != '')
-        <nav class="{{ $class ?? '' }}">
-            {{ $slot }}
-        </nav>
-    @endif
+        @if (isset($slot) && $slot != '')
+            <nav class="{{ $class ?? '' }}">
+                {{ $slot }}
+            </nav>
+        @endif
 
-</header>
+    </header>
 
 
     <main>
         @yield('content')
     </main>
 </body>
+
 </html>
